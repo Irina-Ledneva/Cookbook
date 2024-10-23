2c1da7c93c13f0f60bfe606df4d0f7547c10a806
Irina-Ledneva <111776703+Irina-Ledneva@users.noreply.github.com>
2024-10-23 11:02:44 +0300

    last commit

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mtv29(sm#5wwow8ispj55n@cc&dg)fo6-3t%2_iy96p6le@igc'
+import os
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Ira28cookbook.pythonanywhere.com',]
 
 
 # Application definition
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'cookbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Ira28cookbook$default',
+        'USER': 'Ira28cookbook',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'Ira28cookbook.mysql.pythonanywhere-services.com',
+        'OPTIONS': {'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'", 'charset': 'utf8mb4', },
     }
 }
 
@@ -117,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
